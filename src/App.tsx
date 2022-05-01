1123af3a655ccffe1f7816158099f27c21164d9c
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-05-01 11:15:13 +0200

    Buttons are working

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import {db} from './db/Firebase';
-import { doc, getDoc, collection, getDocs } from "firebase/firestore";
+import { doc, updateDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { motion} from "framer-motion";
 
 
 
 
-function BooleanQuestion({ word }: { word: string }) {
-  function chooseWord() : string{
-  
-    const chosenWord = arrayofDocs[Math.floor(Math.random() * arrayofDocs.length)];
+function BooleanQuestion() {
 
-    return chosenWord;
+  const [chosenWord, setChosenword] = useState({id:'', papiamento:"Cargando, Please warda.",isPapiamento:0,isNotPapiamento:0});
+
+  //function to skip and to also get a docuement
+  const getword = async () => {
+    const max = 16572;
+    const chosenNumber = Math.floor(Math.random() * max);
+    const q = query(collection(db, "Papiamento-data"), where("id", "==", chosenNumber));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      // console.log(doc.id, " => ", doc.data().papiamento);
+      setChosenword({id:doc.data().id, papiamento: doc.data().papiamento,isPapiamento:doc.data().isPapiamento,isNotPapiamento:doc.data().isNotPapiamento})
+    });
+    
+
   }
-  const [arrayofDocs, setArrayofDocs] = useState([]);
-  const [chosenWord, setChosenword] = useState(null);
-  useEffect(()=>{
-    const getInfo = async () => {
-      let arrayOfDocs : any = [];
-      const querySnapshot = await getDocs(collection(db, "Papiamento-data"));
-      querySnapshot.forEach((doc) => {
-        arrayOfDocs.push(doc.data());
-      });
-      
-      setChosenword(arrayOfDocs[Math.floor(Math.random() * arrayOfDocs.length)].papiamento);
-    }
+  const setPapiamento = async () =>{
+    console.log(chosenWord)
+    const ref = doc(db, "Papiamento-data", chosenWord.papiamento);
+    let number = chosenWord.isPapiamento + 1
 
-    getInfo()
-      .catch(console.error)
-  },[]);
+    await updateDoc(ref, {
+      isPapiamento:  number
+    }).then(() => getword())
+
+  }
+  const setNotPapiamento = async () =>{
+    const ref = doc(db, "Papiamento-data", chosenWord.papiamento);
+    let number = chosenWord.isNotPapiamento + 1
+
+    await updateDoc(ref, {
+      isNotPapiamento: number
+    }).then(() => getword())
+    
+
+  }
+
+
+  useEffect(() => {
+    const i = getword()
+      i.catch(console.error)
+  }, []);
 
   return (
     <>
@@ -38,23 +60,23 @@ function BooleanQuestion({ word }: { word: string }) {
           <span className='question'>E palabra of zin aki ta den papiamento?</span>
           <br />
           <br />
-          <span className='word'>{chosenWord}</span>
+          <span className='word'>{chosenWord.papiamento}</span>
         </div>
         <div className='button-div'>
         <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
-             onClick={()=>console.log("yes")}>Si</motion.button>
+             onClick={setPapiamento}>Si</motion.button>
           <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
-             onClick={()=>console.log("no")}>No</motion.button>
+             onClick={setNotPapiamento}>No</motion.button>
         </div>
         <div className='bottom-button-div'>
         <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
-             onClick={()=>console.log("skip")}>Skip</motion.button>
+             onClick={getword}>Skip</motion.button>
         </div>
       </div>
 
@@ -65,7 +87,7 @@ function BooleanQuestion({ word }: { word: string }) {
 function App() {
   return (
     <>
-      <BooleanQuestion word="bo mama" />
+      <BooleanQuestion />
     </>
   );
 }
