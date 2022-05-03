528e20d823a0f95dbecd055f789723efcf6cc9c2
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-05-03 21:13:18 +0200

    main menu done

--- src/App.tsx
+++ src/App.tsx
@@ -1,138 +1,35 @@
-import React,{useCallback, useEffect,useRef,useState} from 'react';
+import React from 'react';
 import './styles/App.css';
 import { BooleanQuestion } from './components/BooleanQuestion';
-import {motion} from 'framer-motion';
-import { db } from './db/Firebase';
-import { doc, updateDoc, collection, getDocs, query, where } from "firebase/firestore";
-import {RoundButton} from './components/RoundButton';
-import { useNavigate } from 'react-router-dom';
-
-
-function SentanceQuestion(){
-  const slidein = {
-    hidden: {
-      x: "-100vh",
-      opacity: 0,
-    },
-    visible: {
-      
-      x: "-50%",
-      opacity: 1,
-      transition: {
-        duration: 1,
-        type: "spring",
-        damping: 20,
-        stiffness: 200,
-      },
-      
-    },
-    exit: {
-      x: "-100vh",
-      opacity: 0,
-      transition: { duration: 0.5 }
-    },
-  };
-  const [chosenWord, setChosenword] = useState({ id: '', papiamento: "Cargando, Please warda.",english:[]});
-  const input = useRef(null);
-  const navigate = useNavigate();
-
-  const enterFunction = useCallback((event) => {
-    if (event.keyCode == 13) {
-      submit();
-      
-    }
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener("keydown", enterFunction);
-
-    return () => {
-      document.removeEventListener("keydown", enterFunction);
-    };
-  }, [enterFunction]);
-  //function to skip and to also get a docuement
-  const getword = async () => {
-    const max = 16571;
-    const chosenNumber = Math.floor(Math.random() * max);
-    //const chosenNumber = 1;
-    const q = query(collection(db, "Papiamento-data"), where("id", "==", chosenNumber));
-
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-     // console.log();
-      setChosenword({ id: doc.data().id, papiamento: doc.data().papiamento,english:doc.data().english });
-    });
-
-
-  };
- 
-
-  const submit = async () => {
-    if(!input.current.value){
-      alert("Please enter a word");
-      return;
-    }
-    const ref = doc(db, "Papiamento-data", chosenWord.papiamento);
-    const data = chosenWord.english;
-    data.push(input.current.value.toLowerCase());
-    await updateDoc(ref, {
-      english: data,
-    }).then(() => {
-      input.current.value = "";
-      getword()
-    });
-  }
-
-  useEffect(() => {
-     //const i = getword();
-      //i.catch(console.error);
-  }, []);
-  return(
-        <>
-        <RoundButton click={()=>navigate('/si-of-no')} />
-      <motion.div
-      variants={slidein}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      className='root'>
-        <div className='header'>
-          <span className='question'>Traduci e palabra of zin aki na Ingles:</span>
-          <br />
-          <br />
-          <motion.span className='word'>{chosenWord.papiamento}</motion.span>
-          <br/>
-          <br/>
-          <motion.input ref={input} type='text' placeholder='Typ e traduccion na ingles kiden' className='text-input'></motion.input>
-        </div>
-        <div className='button-div'>
-          <motion.button
-            type='submit'
-            className='green'
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={submit}>Cla</motion.button>
-
-          <motion.button
-            className='yellow'
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={getword}>Skip</motion.button>
-        </div>
-
-      </motion.div>
-
-    </>
-  );
-}
+import { SentanceQuestion } from './components/SentanceQuestion';
 
 
 
 function App() {
   return (
     <>
-      <SentanceQuestion/>
+      <div className='main-menu-root'>
+      <div className='card'>
+      <div className='top pink-gradient'>
+      <img className='image' src="/yesno.svg" alt="translate" />
+      </div>
+        <h1>Check si un palabra ta den papiamento</h1>
+      </div>
+      <div className='card'>
+      <div className='top yellow-gradient'>
+        <img className='image' src="/translate.svg" alt="translate" />
+      </div>
+        <h1>Traduci e palabra na Ingles</h1>
+      </div>
+      <div className='card'>
+        <div className='top blue-gradient'>
+        <img className='image-social' src="/social.svg" alt="translate" />
+        </div>
+        <h1>Informacion, vision y e mision.</h1>
+      </div>
+      
+      </div>
+
     </>
   );
 }
--- src/components/SentanceQuestion.tsx
+++ src/components/SentanceQuestion.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
 import { db } from '../db/Firebase';
 import { doc, updateDoc, collection, getDocs, query, where } from "firebase/firestore";
-import { motion } from "framer-motion";
-import { useNavigate } from 'react-router-dom';
 import { RoundButton } from './RoundButton';
+import { useNavigate } from 'react-router-dom';
 
-export function BooleanQuestion() {
+export function SentanceQuestion() {
   const slidein = {
     hidden: {
       x: "-100vh",
@@ -27,86 +27,95 @@ export function BooleanQuestion() {
       transition: { duration: 0.5 }
     },
   };
-
-  const [chosenWord, setChosenword] = useState({ id: '', papiamento: "Cargando, Please warda.", isPapiamento: 0, isNotPapiamento: 0 });
+  const [chosenWord, setChosenword] = useState({ id: '', papiamento: "Cargando, Please warda.", english: [] });
+  const input = useRef(null);
   const navigate = useNavigate();
+
+  const enterFunction = useCallback((event) => {
+    if (event.keyCode == 13) {
+      submit();
+
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", enterFunction);
+
+    return () => {
+      document.removeEventListener("keydown", enterFunction);
+    };
+  }, [enterFunction]);
   //function to skip and to also get a docuement
   const getword = async () => {
-    const max = 16572;
+    const max = 16571;
     const chosenNumber = Math.floor(Math.random() * max);
+    //const chosenNumber = 1;
     const q = query(collection(db, "Papiamento-data"), where("id", "==", chosenNumber));
 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      // console.log(doc.id, " => ", doc.data().papiamento);
-      setChosenword({ id: doc.data().id, papiamento: doc.data().papiamento, isPapiamento: doc.data().isPapiamento, isNotPapiamento: doc.data().isNotPapiamento });
+      // console.log();
+      setChosenword({ id: doc.data().id, papiamento: doc.data().papiamento, english: doc.data().english });
     });
 
 
   };
-  const setPapiamento = async () => {
-    console.log(chosenWord);
+
+
+  const submit = async () => {
+    if (!input.current.value) {
+      alert("Please enter a word");
+      return;
+    }
     const ref = doc(db, "Papiamento-data", chosenWord.papiamento);
-    let number = chosenWord.isPapiamento + 1;
-
+    const data = chosenWord.english;
+    data.push(input.current.value.toLowerCase());
     await updateDoc(ref, {
-      isPapiamento: number
-    }).then(() => getword());
-
+      english: data,
+    }).then(() => {
+      input.current.value = "";
+      getword();
+    });
   };
-  const setNotPapiamento = async () => {
-    const ref = doc(db, "Papiamento-data", chosenWord.papiamento);
-    let number = chosenWord.isNotPapiamento + 1;
-
-    await updateDoc(ref, {
-      isNotPapiamento: number
-    }).then(() => getword());
-
-
-  };
-
 
   useEffect(() => {
     const i = getword();
     i.catch(console.error);
   }, []);
-
   return (
     <>
-    <RoundButton click={() => navigate('/')} />
+      <RoundButton click={() => navigate('/si-of-no')} />
       <motion.div
-      variants={slidein}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      className='root'>
+        variants={slidein}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className='root'>
         <div className='header'>
-          <span className='question'>E palabra of zin aki ta den papiamento?</span>
-
+          <span className='question'>Traduci e palabra of zin aki na Ingles:</span>
           <br />
           <br />
           <motion.span className='word'>{chosenWord.papiamento}</motion.span>
+          <br />
+          <br />
+          <motion.input ref={input} type='text' placeholder='Typ e traduccion na ingles kiden' className='text-input'></motion.input>
         </div>
         <div className='button-div'>
           <motion.button
+            type='submit'
             className='green'
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
-            onClick={setPapiamento}>Si</motion.button>
-          <motion.button
-            className='red'
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={setNotPapiamento}>No</motion.button>
-        </div>
-        <div className='bottom-button-div'>
+            onClick={submit}>Cla</motion.button>
+
           <motion.button
             className='yellow'
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
             onClick={getword}>Skip</motion.button>
         </div>
+
       </motion.div>
 
     </>
